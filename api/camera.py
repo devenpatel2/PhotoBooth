957453586388942085b626424a5c6ca126f12d28
D.Patel <d.patel@interproductec.com>
2020-04-09 08:15:50 +0200

    fix webcam index

--- api/camera.py
+++ api/camera.py
@@ -10,7 +10,8 @@ logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-class Camera:
+
+class Camera(ABC):
 
     def __init__(self, camera_type="generic"):
         self._camera_type = camera_type
@@ -31,40 +32,44 @@ class Camera:
         # TO-DO
         pass
 
-class DummyCamera(Camera):
+
+class DummyCam(Camera):
 
     def __init__(self):
         super().__init__(camera_type="Dummy")
 
     def capture(self):
-        rnd_image = np.random.randint(0, 255, [1920, 1024, 3]).astype(dtype=np.uint8)
+        rnd_image = np.random.randint(
+            0, 255, [1024, 1920, 3]).astype(dtype=np.uint8)
         return rnd_image
 
     def close(self):
         pass
 
+
 class WebCam(Camera):
 
     def __init__(self, external=False):
         super().__init__(camera_type="WebCam")
-        device_idx = 0 if external else 1
-        self._camera = cv2.VideoCapture(device_idx)
 
     def close(self):
         self._camera.release()
 
     def capture(self):
+        self._camera = cv2.VideoCapture(-1)
         ret, frame = self._camera.read()
         if not ret:
             raise Exception("Failed to capture image")
+        self._camera.release()
         return frame
 
-class DigitalCamera(Camera):
+
+class DigitalCam(Camera):
 
     def __init__(self):
         super().__init__(camera_type="DigitalCam")
         self._camera = gp.Camera()
-        #self._camera.init()
+        # self._camera.init()
 
     def close(self):
         logger.info("closing camera")
@@ -76,20 +81,23 @@ class DigitalCamera(Camera):
         file_path = self._camera.capture(gp.GP_CAPTURE_IMAGE)
         target = os.path.join(tmp_path, file_path.name)
         camera_file = self._camera.file_get(
-                file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+            file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
         camera_file.save(target)
         image = cv2.imread(target)
         return image
 
     def dummy_image(self):
-        rnd_image = np.random.randint(0, 255, [100, 100, 3]).astype(dtype=np.uint8)
+        rnd_image = np.random.randint(
+            0, 255, [100, 100, 3]).astype(dtype=np.uint8)
         return rnd_image
+
 
 def test_driver():
     cam = Camera()
     image = cam.capture()
     print(image.shape)
     cam.close()
+
 
 if __name__ == "__main__":
 
